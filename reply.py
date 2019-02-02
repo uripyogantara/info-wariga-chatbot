ec100035d8fb27b4a6c17e91f41ace1aab703b5d
Urip Yogantara <uripyogantara@gmail.com>
2019-02-02 21:05:24 +0800

    basis pengetahuan apa

--- reply.py
+++ reply.py
@@ -1,23 +1,32 @@
 from connector import connector
 import re
 from nltk.tokenize import MWETokenizer
+from pprint import pprint
 
-class Enr:
+class reply:
     def __init__(self):
         self.connection=connector().get_connection_object()
-        self.cursor=self.connection.cursor()
+        self.cursor=self.connection.cursor(dictionary=True)
         self.cursor.execute("SELECT * FROM tag")
         data = self.cursor.fetchall()
 
-        self.set_entities(data=data)
+        self.__set_entities(data=data)
 
-        self.cursor.execute("SELECT * FROM basis_hari_raya")
+        self.cursor.execute("SELECT * FROM basis_pengetahuan")
 
         data = self.cursor.fetchall()
 
-        self._basis_hari_raya = {}
+        self._basis_pengetahuan = {}
         for item in data:
-            self._basis_hari_raya[item["nama"]] = item["sql"]
+            self._basis_pengetahuan[item["nama"]] = item["sql"]
+
+        self.cursor.execute("SELECT * FROM basis_pengetahuan_apa")
+
+        data = self.cursor.fetchall()
+
+        self._basis_pengetahuan_apa = {}
+        for item in data:
+            self._basis_pengetahuan_apa[item["nama"]] = item["deskripsi"]
 
         self.cursor.execute("SELECT * FROM padanan")
 
@@ -27,20 +36,23 @@ class Enr:
         for item in data:
             self._padanan[item["value"]] = item["default"]
 
-    def set_entities(self,data):
+
+    def __set_entities(self,data):
         entities={}
         for item in data:
+            # print(item)
             entities[item["word"]]=item["tag"]
 
         self._entities=entities
 
-    def tokenize(self,msg):
+    def __tokenize(self,msg):
         msg = re.sub(r'[^\w]', ' ', msg)
         tokenizer = MWETokenizer()
         tokenizer.add_mwe(("buda", "wage"))
         token = tokenizer.tokenize(msg.split())
+        return token
 
-    def get_enr(self,tokens):
+    def __get_enr(self,tokens):
         enr = {}
         for token in tokens:
             if token in self._entities:
@@ -49,19 +61,13 @@ class Enr:
                 enr[token] = "tahun"
         return enr
 
-    def get_entities(self):
-        return self._entities
-
-    def get_basis_hari_raya(self):
-        return self._basis_hari_raya
-
-    def get_padanan(self,value):
+    def __get_padanan(self,value):
         if value in self._padanan:
             return self._padanan[value]
         else:
             return value
 
-    def get_response(result):
+    def __get_response(self,result):
         intent = None
         responses = []
         # for val in values:
@@ -125,50 +131,60 @@ class Enr:
                     }
         return responses
 
-    def result(self,responses):
+    def __result(self,responses):
         hasil = []
+
         for response in responses:
             if (response["intent"] == "search_when"):
                 sql = "SELECT * FROM kalender WHERE tanggal>DATE(NOW())"
 
                 if "hari_raya" in response:
-                    print("cari hari raya %s" % (response["hari_raya"]))
-                    sql += " and " + self._basis_hari_raya[response["hari_raya"]]
+                    sql += " and " + self._basis_pengetahuan[response["hari_raya"]]
                 elif "dewasa_ayu" in response:
-                    print("cari dewasa %s" % (response["dewasa_ayu"]))
+                    sql += " and " + self._basis_pengetahuan[response["dewasa_ayu"]]
                 for entity in response["entities"]:
-                    # if response["entities"][entity]["negation"]:
+                    data = self.__join(response["entities"][entity]["data"])
 
-                    data = self.join(response["entities"][entity]["data"])
-                    sql += " and %s in (%s)" % (entity, data)
-                    # print(data)
-                    # print(response["entities"][entity])
-                # print(sql)
-                reply = self.get_reply(sql)
-                print(reply)
+                    if response["entities"][entity]["negation"]==True:
+                        sql += " and %s not in (%s)" % (entity, data)
+                    else:
+                        sql += " and %s in (%s)" % (entity, data)
+                print(sql)
+                reply = self.__get_reply(sql)
+                hasil.append(str(reply["tanggal"]))
             elif (response["intent"] == "search_what"):
                 hari_raya = ""
-                for entity in response["entities"]:
-                    if response["entities"][entity] == "hari_raya":
-                        hari_raya = entity
-                hasil.append("kamu menanyakan apa %s" % hari_raya)
-                print(response)
+                if "hari_raya" in response:
+                    hari_raya=self._basis_pengetahuan_apa[response['hari_raya']]
+                hasil.append(hari_raya)
+                # print(response)
 
         return hasil
 
-    def join(self,data):
+    def __join(self,data):
         sql = ""
         for i, item in enumerate(data):
-            item = "'" + self.get_padanan(item) + "'"
+            item = "'" + self.__get_padanan(item) + "'"
             if i < len(data) - 1:
                 sql += item + ","
             else:
                 sql += item
         return sql
 
-    def get_reply(self,sql):
+    def __get_reply(self,sql):
         self.cursor.execute(sql)
         data=self.cursor.fetchone()
+        self.connection.rollback()
         return data
+
+    def get_reply(self,msg):
+        token=self.__tokenize(msg)
+        # print(token)
+        enr = self.__get_enr(token)
+        responses = self.__get_response(enr)
+
+        # pprint(responses)
+        result=self.__result(responses)
+        return result
 
 
